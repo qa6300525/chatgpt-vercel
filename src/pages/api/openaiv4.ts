a29f3de7f9f864f3bb44f38ef0846381f28b3445
程茂宇 <chengmaoyu@100.me>
2023-04-01 11:47:44 +0800

    test

---
+++ src/pages/api/openaiv4.ts
@@ -0,0 +1,72 @@
+export async function post(context) {
+  try {
+    // 获取请求的信息
+    const { url, headers, method, body } = context.request;
+    // 返回一个JSON对象
+    const target = "https://api.openai.com/v1/chat/completions";
+    // 转发请求并获取响应
+    const response = await fetch(target, { headers, method, body });
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data
+      }),
+    };
+  } catch (error) {
+    // 打印错误信息
+    console.error(error);
+    // 返回一个错误响应
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: "Something went wrong",
+        error: error.message,
+      }),
+    };
+  }
+}
+
+
+export async function get(context) {
+  try {
+    // 获取请求的信息
+    const { url, headers, method, body } = context.request;
+    // 返回一个JSON对象
+    const target = "https://api.openai.com/v1/chat/completions";
+    // 转发请求并获取响应
+    const response = await fetch(target, { headers, method, body });
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data
+      }),
+    };
+  } catch (error) {
+    // 打印错误信息
+    console.error(error);
+    // 返回一个错误响应
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: "Something went wrong",
+        error: error.message,
+      }),
+    };
+  }
+}
